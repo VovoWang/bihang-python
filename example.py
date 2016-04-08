e315759109cb2834d616891282ea4f96bdc6cd27
bihanggit <github@bihang.com>
2016-04-08 15:44:46 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-from oklink.api import OklinkClient
+from bihang.api import Bihang
 
-client = OklinkClient.api_secret(api_key="", api_secret="")
+client = Bihang.api_secret(api_key="", api_secret="")
 
 r = client.buttonsButton({'name':"python button",'price':2,'price_currency':'BTC'})
 
